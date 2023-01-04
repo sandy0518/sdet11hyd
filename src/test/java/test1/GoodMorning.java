d57f08c2428720602dff125e41d92d385e909e3f
sandy0518 <sai.sandeep.906@gmail.com>
2023-01-04 16:10:34 +0530

    wishes are created as java program

---
+++ src/test/java/test1/GoodMorning.java
@@ -0,0 +1,10 @@
+package test1;
+
+public class GoodMorning {
+
+	public static void main(String[] args) {
+		System.out.println("Good morning");
+
+	}
+
+}
---
+++ src/test/java/test1/GoodNight.java
@@ -0,0 +1,8 @@
+package test1;
+
+public class GoodNight {
+
+	public static void main(String[] args) {
+		System.out.println("Good Night");
+	}
+}
